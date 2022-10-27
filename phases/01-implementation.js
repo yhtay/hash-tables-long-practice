451adefb83df3eaa36915d00640bde399310d1b4
Ye Lwin Htay <yelwin.r.k@gmail.com>
2022-10-27 15:03:01 -0700

    01-implementation almost complete

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -37,6 +37,11 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
     const newPair = new KeyValuePair(key, value);
     const index = this.hashMod(key);
 
+    let loadFactor = this.count / this.capacity;
+    if (loadFactor >= 0.7) {
+      this.resize();
+    }
+
     if (!this.data[index]) {
       this.data[index] = newPair;
       this.count++;
@@ -104,5 +109,15 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
   }
 }
 
+const hashTable = new HashTable(4);
+
+hashTable.insert("key1", "value1")
+hashTable.insert("key2", "value2")
+hashTable.insert("key3", "value3")
+hashTable.insert("key5", "value5")
+hashTable.insert("key9", "value9")
+hashTable.insert("key10", "value10")
+
+console.log(hashTable.read("key5"))
 
 module.exports = HashTable;
