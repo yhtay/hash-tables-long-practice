a947df62167fd518bde6f7b83f76c87c6f159918
Ye Lwin Htay <yelwin.r.k@gmail.com>
2022-10-27 14:21:48 -0700

    prtactice almost completed

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -11,6 +11,9 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
   constructor(numBuckets = 8) {
     // Initialize your buckets here
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null)
   }
 
   hash(key) {
@@ -31,16 +34,68 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   insert(key, value) {
     // Your code here
+    const newPair = new KeyValuePair(key, value);
+    const index = this.hashMod(key);
+
+    if (!this.data[index]) {
+      this.data[index] = newPair;
+      this.count++;
+    }
+    let current = this.data[index];
+    while (current) {
+      if (current.key === key) {
+        current.value = value;
+        return;
+      }
+
+      current = current.next;
+    }
+    newPair.next = this.data[index]
+    this.data[index] = newPair;
+    this.count++
   }
 
 
   read(key) {
     // Your code here
+    const index = this.hashMod(key);
+    let current = this.data[index];
+
+    while (current) {
+      if (current.key === key) {
+        return current.value
+      }
+      current = current.next;
+    }
+    return undefined;
+
   }
 
 
   resize() {
     // Your code here
+    const storedData = {}
+    for (let i = 0; i < this.data.length; i++) {
+      let current = this.data[i];
+      // console.log(current);
+
+      while (current) {
+        storedData[current.key] = current.value;
+        current = current.next;
+        // console.log(storedData)
+      }
+    }
+    this.capacity = this.capacity * 2;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
+    // console.log(storedData)
+    for (let pair in storedData) {
+      // console.log(pair)
+      // console.log(storedData[pair])
+      this.insert(pair, storedData[pair])
+      // console.log(this.data)
+
+    }
   }
 
 
