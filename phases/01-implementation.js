ca1199541a5fe9389df164f5e2c404266c959308
jlin231 <jonlin981@gmail.com>
2022-10-27 16:55:31 -0700

    tried delete

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -31,20 +31,15 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
     return this.hash(key) % this.capacity;
   }
 
-
   insert(key, value) {
     // Your code here
+    if (this.count / this.capacity >= 0.7) this.resize();
     const newPair = new KeyValuePair(key, value);
     const index = this.hashMod(key);
-
-    let loadFactor = this.count / this.capacity;
-    if (loadFactor >= 0.7) {
-      this.resize();
-    }
-
     if (!this.data[index]) {
       this.data[index] = newPair;
       this.count++;
+      return;
     }
     let current = this.data[index];
     while (current) {
@@ -52,7 +47,6 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
         current.value = value;
         return;
       }
-
       current = current.next;
     }
     newPair.next = this.data[index]
@@ -60,30 +54,26 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
     this.count++
   }
 
-
   read(key) {
     // Your code here
     const index = this.hashMod(key);
     let current = this.data[index];
-
     while (current) {
       if (current.key === key) {
         return current.value
       }
       current = current.next;
     }
+    
     return undefined;
-
   }
-
-
   resize() {
     // Your code here
     const storedData = {}
     for (let i = 0; i < this.data.length; i++) {
       let current = this.data[i];
       // console.log(current);
-
+      
       while (current) {
         storedData[current.key] = current.value;
         current = current.next;
@@ -93,31 +83,63 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
     this.capacity = this.capacity * 2;
     this.data = new Array(this.capacity).fill(null);
     this.count = 0;
-    // console.log(storedData)
     for (let pair in storedData) {
       // console.log(pair)
       // console.log(storedData[pair])
       this.insert(pair, storedData[pair])
       // console.log(this.data)
-
     }
+    
   }
 
 
   delete(key) {
-    // Your code here
+    const index = this.hashMod(key)
+    console.log(this.data)
+    if(!this.data[index]){
+      return;
+    }
+    //if first linked list is the key
+    if(this.data[index].key === key){
+      if(this.data[index] === null) {
+        this.data[index] = null;
+        return;
+      }
+      this.data[index].next = this.data[index].next.next;
+      return;
+    }
+    //
+    let current = this.data[index];
+    while(current){
+      //if current.next is end of the linked list, check current.next is the to be deleted item
+      //set current to point to null 
+      console.log(this.data)
+      if(!current.next.next){
+        if(current.next.key === key){
+          current.next = null;
+          return
+        }
+        return
+      }
+      //
+      if(current.next.key === key){
+        current.next = current.next.next; 
+        return
+      }
+      current = current.next;
+    }
   }
 }
 
-const hashTable = new HashTable(4);
+// const hashTable = new HashTable(4);
 
-hashTable.insert("key1", "value1")
-hashTable.insert("key2", "value2")
-hashTable.insert("key3", "value3")
-hashTable.insert("key5", "value5")
-hashTable.insert("key9", "value9")
-hashTable.insert("key10", "value10")
+// hashTable.insert("key1", "value1");
+// hashTable.insert("key2", "value2");
+// hashTable.insert("key3", "value3");
+// hashTable.insert("key5", "value5");
+// hashTable.insert("key9", "value9")
+// hashTable.insert("key10", "value10")
 
-console.log(hashTable.read("key5"))
-
+// console.log(hashTable.read("key5"));
+// console.log(hashTable.data);
 module.exports = HashTable;
